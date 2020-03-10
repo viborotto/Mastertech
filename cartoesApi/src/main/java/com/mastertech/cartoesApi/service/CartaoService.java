4b76fb6d31bbf90e69f95ed3c56ef188010422aa
viborotto <vittoriaborotto@gmail.com>
2020-03-10 09:56:16 -0300

    Add Services and TODO's

---
+++ cartoesApi/src/main/java/com/mastertech/cartoesApi/service/CartaoService.java
@@ -0,0 +1,8 @@
+package com.mastertech.cartoesApi.service;
+
+public class CartaoService {
+
+    //TODO Não deve ser possivel criar um cartão de um cliente que não exite
+    //TODO Ativar cartao
+    //TODO consultar fatura
+}
---
+++ cartoesApi/src/main/java/com/mastertech/cartoesApi/service/ClienteService.java
@@ -0,0 +1,8 @@
+package com.mastertech.cartoesApi.service;
+
+public class ClienteService {
+
+    //TODO fazer uma compra com o cartao | Não deve ser possivel fazer uma compra em um cartão que não existe
+    //TODO Não deve ser possivel fazer uma compra em um cartão que não está ativo
+
+}

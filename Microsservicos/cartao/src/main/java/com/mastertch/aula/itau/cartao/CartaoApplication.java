5a41b35ccf7239620902bf95c324f888f3a7b195
viborotto <vittoriaborotto@gmail.com>
2020-03-12 09:55:07 -0300

    Cliente registrado no Eureka

--- Microsservicos/cartao/src/main/java/com/mastertch/aula/itau/cartao/CartaoApplication.java
+++ Microsservicos/cartao/src/main/java/com/mastertch/aula/itau/cartao/CartaoApplication.java
@@ -2,8 +2,10 @@ package com.mastertch.aula.itau.cartao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 //passar todo o cartao pra esse projeto
 
+@EnableFeignClients
 @SpringBootApplication
 public class CartaoApplication {
 

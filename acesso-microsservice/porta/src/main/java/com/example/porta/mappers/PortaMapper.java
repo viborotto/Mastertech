e9311b329193aed5471fb2a1f8b740450a55c7bb
viborotto <vittoriaborotto@gmail.com>
2020-03-13 16:30:59 -0300

    CreatePortaRequest & CreatePortaResponse

--- acesso-microsservice/porta/src/main/java/com/example/porta/mappers/PortaMapper.java
+++ acesso-microsservice/porta/src/main/java/com/example/porta/mappers/PortaMapper.java
@@ -1,9 +1,32 @@
 package com.example.porta.mappers;
 
 
+import com.example.porta.dtos.request.CreatePortaRequest;
+import com.example.porta.dtos.response.CreatePortaResponse;
+import com.example.porta.models.Porta;
+import org.springframework.stereotype.Component;
 
+//
+@Component
 public class PortaMapper {
-    
 
+    //portarequest em porta, porta em response
+    public Porta fromCreatePortaRequestToPorta(CreatePortaRequest createPortaRequest) {
+        Porta porta = new Porta();
+
+        porta.setAndar(createPortaRequest.getAndar());
+        porta.setSala(createPortaRequest.getSala());
+
+        return porta;
+    }
+
+    public CreatePortaResponse fromPortaToCreatePortaResponse(Porta porta){
+        CreatePortaResponse createPortaResponse = new CreatePortaResponse();
+
+        createPortaResponse.setSala(porta.getSala());
+        createPortaResponse.setAndar(porta.getAndar());
+
+        return createPortaResponse;
+    }
 
 }
--- acesso-microsservice/porta/src/main/java/com/example/porta/services/PortaService.java
+++ acesso-microsservice/porta/src/main/java/com/example/porta/services/PortaService.java
@@ -1,4 +1,15 @@
 package com.example.porta.services;
 
+
+import com.example.porta.repositories.PortaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//vamos usar os Requests e Response criados aqui
+@Service
 public class PortaService {
+
+    @Autowired
+    private PortaRepository portaRepository;
+
 }
